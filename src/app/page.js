1756215d9776bba6f2ce45aed19f74de5026c8b7
kushadarsh <adarshkush1311@gmail.com>
2024-07-18 01:45:51 +0530

    App Final commit

--- src/app/page.js
+++ src/app/page.js
@@ -32,7 +32,7 @@ export default function Home() {
 
     const queryString = new URLSearchParams(formData).toString();
     try {
-      const res = await fetch(`https://3.142.159.72:443/urls?${queryString}`, {
+      const res = await fetch(`https://aiagentlb.com/urls?${queryString}`, {
         method: 'GET',
       });
       const data = await res.json();
